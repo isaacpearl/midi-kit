ecc760e7e612b2101ffacba815e608e1e2a7a120
Isaac Pearl <pearl.isaac@gmail.com>
2023-02-20 21:10:19 -0500

    print note values

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -18,6 +18,8 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     MidiMonitor.setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x32ffffff));
     MidiMonitor.setColour (juce::TextEditor::outlineColourId, juce::Colour (0x1c000000));
     MidiMonitor.setColour (juce::TextEditor::shadowColourId, juce::Colour (0x16000000));
+
+    startTimerHz(24);
     setSize (600, 400);
 }
 
@@ -28,13 +30,11 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 void AudioPluginAudioProcessorEditor::timerCallback()
 {
     std::queue<juce::MidiMessageMetadata>* midiMessages = processorRef.getMidiMessages();
-    juce::MidiMessageMetadata rawMsg = midiMessages->front();
-    midiMessages->pop();
-    if (!rawMsg) {
-        return;
-    } else {
+    if (!midiMessages->empty()) {
+        juce::MidiMessageMetadata rawMsg = midiMessages->front();
+        midiMessages->pop();
         juce::MidiMessage msg = rawMsg.getMessage();
-        MidiMonitor.logMessage(msg.getNoteNumber()); 
+        MidiMonitor.logMessage(std::to_string(msg.getNoteNumber())); 
     }
 }
 
@@ -52,5 +52,5 @@ void AudioPluginAudioProcessorEditor::resized()
 {
     auto area = getLocalBounds();
     MidiMonitor.setBounds (area.reduced (8));
-    //MidiMonitor.logMessage(area.toString());
+    MidiMonitor.logMessage("adsfa"); 
 }
